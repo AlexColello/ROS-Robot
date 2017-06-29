f37b69e229b766b89570c39af6d6aedfede0ee3e
Your Name <you@example.com>
2017-06-29 04:43:37 +0000

    Added variables for all necessary registers and cleaned up unnecessary methods

--- PWMDriver.py
+++ PWMDriver.py
@@ -1,52 +1,63 @@
 import smbus
 import time
 import math
-
-PCA9685_MODE1 = 0x0
-PCA9685_PRESCALE = 0xFE
+import RPi.GPIO as gpio
 
 
 class PWMDriver:
+ 
+  PCA9685_MODE1 = 0x00
+  PCA9685_PRESCALE = 0xFE
+  PCA9685_RA_MODE2          = 0x01
+  PCA9685_RA_LED0_ON_L      = 0x06
+  PCA9685_RA_LED0_ON_H      = 0x07
+  PCA9685_RA_LED0_OFF_L     = 0x08
+  PCA9685_RA_LED0_OFF_H     = 0x09
+  PCA9685_RA_ALL_LED_ON_L   = 0xFA
+  PCA9685_RA_ALL_LED_ON_H   = 0xFB
+  PCA9685_RA_ALL_LED_OFF_L  = 0xFC
+  PCA9685_RA_ALL_LED_OFF_H  = 0xFD
 
+  PCA9685_MODE1_RESTART_BIT = (1 << 7)
+  PCA9685_MODE1_EXTCLK_BIT  = (1 << 6)
+  PCA9685_MODE1_AI_BIT      = (1 << 5)
+  PCA9685_MODE1_SLEEP_BIT   = (1 << 4)
+  PCA9685_MODE1_SUB1_BIT    = (1 << 3)
+  PCA9685_MODE1_SUB2_BIT    = (1 << 2)
+  PCA9685_MODE1_SUB3_BIT    = (1 << 1)
+  PCA9685_MODE1_ALLCALL_BIT = (1 << 0)
+  PCA9685_ALL_LED_OFF_H_SHUT = (1 << 4)
+  PCA9685_MODE2_INVRT_BIT   = (1 << 4)
+  PCA9685_MODE2_OCH_BIT     = (1 << 3)
+  PCA9685_MODE2_OUTDRV_BIT  = (1 << 2)
+  PCA9685_MODE2_OUTNE1_BIT  = (1 << 1)
+  PCA9685_MODE2_OUTNE0_BIT  = (1 << 0)
+  
   def __init__(self, bus):
     self._i2caddr = 0x40
     self.bus = bus
-
-  def begin(self) :
-    global PCA9685_MODE1
-    global PCA9685_PRESCALE
-
     self.reset()
-    self.setPWMFreq(1500)
+    self.setPWMFreq(50)
 
 
   def reset(self) :
-    self.write8(PCA9685_MODE1, 0x0)
+    self.write8(self.PCA9685_MODE1, 0x0)
     
 
   def setPWMFreq(self, freq) :
-    #Serial.print("Attempting to set freq ")
-    #Serial.println(freq)
-    freq *= 0.9  # Correct for overshoot in the frequency setting (see issue #11).
-    self.prescaleval = 25000000
-    self.prescaleval /= 4096
-    self.prescaleval /= freq
-    self.prescaleval -= 1
-    #Serial.print("Estimated pre-scale: ") Serial.println(prescaleval)
-     
-    self.prescale = int(math.floor(self.prescaleval + 0.5))
-    #Serial.print("Final pre-scale: ") Serial.println(prescale)
+    print "Attempting to set freq ", freq
+    self.prescaleval = int(math.ceil(24576000 / (4096 * freq))) - 1
+    print "Final pre-scale: " , self.prescaleval
      
     
-    self.oldmode = self.read8(PCA9685_MODE1)
+    self.oldmode = self.read8(self.PCA9685_MODE1)
     self.newmode = (self.oldmode&0x7F) | 0x10 # sleep
-    self.write8(PCA9685_MODE1, self.newmode) # go to sleep
-    self.write8(PCA9685_PRESCALE, self.prescale) # set the prescaler
-    self.write8(PCA9685_MODE1, self.oldmode)
-    time.sleep(.005)
-    self.write8(PCA9685_MODE1, self.oldmode | 0xa1)  #  This sets the MODE1 register to turn on auto increment.
+    self.write8(self.PCA9685_MODE1, self.newmode) # go to sleep
+    self.write8(self.PCA9685_PRESCALE, self.prescaleval) # set the prescaler
+    self.write8(self.PCA9685_MODE1, self.oldmode)
+    self.write8(self.PCA9685_MODE1, self.oldmode | 0xa1)  #  This sets the MODE1 register to turn on auto increment.
                                             # This is why the beginTransmission below was not working.
-    #  Serial.print("Mode now 0x") Serial.println(read8(PCA9685_MODE1), HEX)
+    print "Mode now 0x", self.read8(self.PCA9685_MODE1)
    
 
   def setPWM(self, num, on, off) :
@@ -112,11 +123,10 @@ class PWMDriver:
     self.bus.write_byte_data(self._i2caddr, addr, d)
 
 pwmdriver = PWMDriver(smbus.SMBus(1))
-pwmdriver.begin()
 
 while True:
   print("Set pins forward")
-  for i in range(0,16):
+  for i in range(0,13):
     pwmdriver.setPin(i, 3072)
   time.sleep(2)
   print("Set pins backwards")
