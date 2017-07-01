bda1af9b7e9b674a8f68545a16d0aaf36a3e5cee
Your Name <you@example.com>
2017-07-01 04:17:28 +0000

    Cleaned up the variable names and made the setPWMFreq method match the Ardupilot version instead of the Adafruit driver.

--- PWMDriver.py
+++ PWMDriver.py
@@ -5,36 +5,38 @@ import RPi.GPIO as gpio
 
 
 class PWMDriver:
-  FREQUENCY = 100
-  PCA9685_MODE1 = 0x00
-  PCA9685_PRESCALE = 0xFE
-  PCA9685_RA_MODE2          = 0x01
-  PCA9685_RA_LED0_ON_L      = 0x06
-  PCA9685_RA_LED0_ON_H      = 0x07
-  PCA9685_RA_LED0_OFF_L     = 0x08
-  PCA9685_RA_LED0_OFF_H     = 0x09
-  PCA9685_RA_ALL_LED_ON_L   = 0xFA
-  PCA9685_RA_ALL_LED_ON_H   = 0xFB
-  PCA9685_RA_ALL_LED_OFF_L  = 0xFC
-  PCA9685_RA_ALL_LED_OFF_H  = 0xFD
 
-  PCA9685_MODE1_RESTART_BIT = (1 << 7)
-  PCA9685_MODE1_EXTCLK_BIT  = (1 << 6)
-  PCA9685_MODE1_AI_BIT      = (1 << 5)
-  PCA9685_MODE1_SLEEP_BIT   = (1 << 4)
-  PCA9685_MODE1_SUB1_BIT    = (1 << 3)
-  PCA9685_MODE1_SUB2_BIT    = (1 << 2)
-  PCA9685_MODE1_SUB3_BIT    = (1 << 1)
-  PCA9685_MODE1_ALLCALL_BIT = (1 << 0)
-  PCA9685_ALL_LED_OFF_H_SHUT = (1 << 4)
-  PCA9685_MODE2_INVRT_BIT   = (1 << 4)
-  PCA9685_MODE2_OCH_BIT     = (1 << 3)
-  PCA9685_MODE2_OUTDRV_BIT  = (1 << 2)
-  PCA9685_MODE2_OUTNE1_BIT  = (1 << 1)
-  PCA9685_MODE2_OUTNE0_BIT  = (1 << 0)
+  FREQUENCY = 100
+
+  PRESCALE          = 0xFE
+  RA_MODE1          = 0x00
+  RA_MODE2          = 0x01
+  RA_LED0_ON_L      = 0x06
+  RA_LED0_ON_H      = 0x07
+  RA_LED0_OFF_L     = 0x08
+  RA_LED0_OFF_H     = 0x09
+  RA_ALL_LED_ON_L   = 0xFA
+  RA_ALL_LED_ON_H   = 0xFB
+  RA_ALL_LED_OFF_L  = 0xFC
+  RA_ALL_LED_OFF_H  = 0xFD
+
+  MODE1_RESTART_BIT = (1 << 7)
+  MODE1_EXTCLK_BIT  = (1 << 6)
+  MODE1_AI_BIT      = (1 << 5)
+  MODE1_SLEEP_BIT   = (1 << 4)
+  MODE1_SUB1_BIT    = (1 << 3)
+  MODE1_SUB2_BIT    = (1 << 2)
+  MODE1_SUB3_BIT    = (1 << 1)
+  MODE1_ALLCALL_BIT = (1 << 0)
+  ALL_LED_OFF_H_SHUT = (1 << 4)
+  MODE2_INVRT_BIT   = (1 << 4)
+  MODE2_OCH_BIT     = (1 << 3)
+  MODE2_OUTDRV_BIT  = (1 << 2)
+  MODE2_OUTNE1_BIT  = (1 << 1)
+  MODE2_OUTNE0_BIT  = (1 << 0)
   
-  def __init__(self, bus):
-    self._i2caddr = 0x40
+  def __init__(self, bus, addr=0x40):
+    self._i2caddr = addr
     self.bus = bus
     self.reset()
     self.setPWMFreq(self.FREQUENCY)
@@ -45,23 +47,20 @@ class PWMDriver:
 
 
   def reset(self) :
-    self.write8(self.PCA9685_MODE1, 0x0)
+    self.write8(self.RA_MODE1, 0x0)
     
 
   def setPWMFreq(self, freq) :
     print "Attempting to set freq ", freq
     self.prescaleval = int(math.ceil(24576000 / (4096 * freq))) - 1
     print "Final pre-scale: " , self.prescaleval
-     
-    
-    self.oldmode = self.read8(self.PCA9685_MODE1)
-    self.newmode = (self.oldmode&0x7F) | 0x10 # sleep
-    self.write8(self.PCA9685_MODE1, self.newmode) # go to sleep
-    self.write8(self.PCA9685_PRESCALE, self.prescaleval) # set the prescaler
-    self.write8(self.PCA9685_MODE1, self.oldmode)
-    self.write8(self.PCA9685_MODE1, self.oldmode | 0xa1)  #  This sets the MODE1 register to turn on auto increment.
-                                            # This is why the beginTransmission below was not working.
-    print "Mode now 0x", self.read8(self.PCA9685_MODE1)
+
+    self.write8(self.RA_ALL_LED_OFF_H, self.ALL_LED_OFF_H_SHUT) # shutdown before sleeping
+    self.write8(self.RA_MODE1, self.MODE1_SLEEP_BIT) # go to sleep
+    self.write8(self.PRESCALE, self.prescaleval) # set the prescaler
+    #self.write8(self.RA_MODE1, self.MODE1_SLEEP_BIT | self.MODE1_EXTCLK_BIT)
+    self.write8(self.RA_MODE1, self.MODE1_RESTART_BIT | self.MODE1_AI_BIT)  #  This sets the MODE1 register to turn on auto increment.
+    print "Mode now 0x", self.read8(self.RA_MODE1)
    
 
   def setPWM(self, num, on, off) :
@@ -107,23 +106,9 @@ class PWMDriver:
         self.setPWM(num, 0, val)
        
   def read8(self, addr):
-    """
-    WIRE.beginTransmission(_i2caddr)
-    WIRE.write(addr)
-    WIRE.endTransmission()
-
-    WIRE.requestFrom((uint8_t)_i2caddr, (uint8_t)1)
-    return WIRE.read()
-    """
     return self.bus.read_byte_data(self._i2caddr, addr)
 
   def write8(self, addr, d):
-    """
-    WIRE.beginTransmission(_i2caddr)
-    WIRE.write(addr)
-    WIRE.write(d)
-    WIRE.endTransmission()
-    """
     self.bus.write_byte_data(self._i2caddr, addr, d)
 
 pwmdriver = PWMDriver(smbus.SMBus(1))
