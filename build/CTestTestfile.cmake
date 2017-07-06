b55b8cb98b3854d441e2b3263b763baf0b7cee0e
Your Name <you@example.com>
2017-07-06 02:48:04 +0000

    Made the directory into a catkin workspace.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/erle/ROS-Robot/src
+# Build directory: /home/erle/ROS-Robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
