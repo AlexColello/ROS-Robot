0b4cd82249f5c6e02bf4a871e7a84a5fe0195888
2141-Alex <alexlcolello@gmail.com>
2017-07-12 16:17:41 -0700

    Completed the basics of this node

--- src/teleoperated_control/src/teleop_control.py
+++ src/teleoperated_control/src/teleop_control.py
@@ -2,60 +2,57 @@
 
 import rospy
 from sensor_msgs import Joy
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 
-def calc_arcade_drive(moveValue, rotateValue):
-    
-    #square the inputs (while preserving the sign) to increase fine
-    #control
-    if moveValue >= 0.0:
-        moveValue = moveValue * moveValue
-    else:
-        moveValue = -(moveValue * moveValue)
-    
-    if rotateValue >= 0.0:
-        rotateValue = rotateValue * rotateValue
-    else:
-        rotateValue = -(rotateValue * rotateValue)
-
-    if moveValue > 0.0:
-        if rotateValue > 0.0:
-            leftMotorSpeed = moveValue - rotateValue
-            rightMotorSpeed = Math.max(moveValue, rotateValue)
-        else:
-            leftMotorSpeed = Math.max(moveValue, -rotateValue)
-            rightMotorSpeed = moveValue + rotateValue
-    else:
-        if rotateValue > 0.0:
-            leftMotorSpeed = -Math.max(-moveValue, rotateValue)
-            rightMotorSpeed = moveValue + rotateValue
-        else:
-            leftMotorSpeed = moveValue - rotateValue
-            rightMotorSpeed = -Math.max(-moveValue, -rotateValue)
-            
-    return leftMotorSpeed, rightMotorSpeed
+class TeleopDrive:
         
+    def calc_arcade_drive(self, moveValue, rotateValue):
 
-def callback(data):
-    x = data.axes[2]
-    y = data.axes[3]
-    left_motor, right_motor = calc_arcade_drive(y, x)
+        #square the inputs (while preserving the sign) to increase fine
+        #control
+        if moveValue >= 0.0:
+            moveValue = moveValue * moveValue
+        else:
+            moveValue = -(moveValue * moveValue)
 
-def teleop_control():
-    
-    pub = rospy.Publisher('pwm_tester', Float32, queue_size=10)
-    rospy.init_node('pwm_tester', anonymous=True)
-    rate = rospy.Rate(100) # 100hz
-    value = .5
-    rospy.Subscriber('joy_node', Joy, callback)
-    while not rospy.is_shutdown():
-        value *= 1.0
-        rospy.loginfo(value)
-        pub.publish(value)
-        rate.sleep()
+        if rotateValue >= 0.0:
+            rotateValue = rotateValue * rotateValue
+        else:
+            rotateValue = -(rotateValue * rotateValue)
+
+        if moveValue > 0.0:
+            if rotateValue > 0.0:
+                leftMotorSpeed = moveValue - rotateValue
+                rightMotorSpeed = Math.max(moveValue, rotateValue)
+            else:
+                leftMotorSpeed = Math.max(moveValue, -rotateValue)
+                rightMotorSpeed = moveValue + rotateValue
+        else:
+            if rotateValue > 0.0:
+                leftMotorSpeed = -Math.max(-moveValue, rotateValue)
+                rightMotorSpeed = moveValue + rotateValue
+            else:
+                leftMotorSpeed = moveValue - rotateValue
+                rightMotorSpeed = -Math.max(-moveValue, -rotateValue)
+
+        return leftMotorSpeed, rightMotorSpeed
+
+
+    def callback(self,data):
+        x = data.axes[2]
+        y = data.axes[3]
+        left_motor, right_motor = calc_arcade_drive(y, x)
+        self.pub.publish([left_motor, right_motor, 0,0,0,0,0,0,0,0,0,0,0])
+
+    def __init__(self):
+        self.pub = rospy.Publisher('pwm_tester', Float32MultiArray, queue_size=10)
+        rospy.init_node('pwm_tester', anonymous=True)
+        rospy.Subscriber('joy_node', Joy, callback)
+        self.pub.publish([0,0,0,0,0,0,0,0,0,0,0,0,0])
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
-        teleop_control()
+        teleop_drive = TeleopDrive()
     except rospy.ROSInterruptException:
         pass
