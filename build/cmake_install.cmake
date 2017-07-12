8c146839bd5858b896ef2d7619b3d456b1d08853
Your Name <you@example.com>
2017-07-12 04:59:36 +0000

    Created a new package that will allow teleoperated control.

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/erle/ROS-Robot/build/pwm_control/cmake_install.cmake")
+  include("/home/erle/ROS-Robot/build/teleoperated_control/cmake_install.cmake")
 
 endif()
 
